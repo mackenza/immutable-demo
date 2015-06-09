8c40b90a5243fd4f6c2595a320bef0d98b41817a
mackenza <a.mackenzie@gmail.com>
2015-06-08 21:34:03 -0400

    more changes

--- contrived-comment-demo.js
+++ contrived-comment-demo.js
@@ -2,15 +2,20 @@ var Immutable   = require("immutable"),
     read        = require("read");
 
 
-var blogPost = {
+var blogPost = Immutable.Map({
     author: "Andrew MacKenzie",
     published: true,
     publishDate: "2015-05-15",
-    comments: {}
+    contents: "The quick brown fox jumps over the lazy dog."
+});
+
+var commentHandler = {
+    add: function (theBlogPost, newContent) {
+        newContents = blogPost.set('contents', blogPost.get('contents') + " " + newContent);
+        return newContents;
+    },
+    render: function(currentBlogPost) {
+        console.log(currentBlogPost);
+    }
 };
 
-var commentStuff = {
-    add: function(blogPost.comments) {
-
-    }
-}
